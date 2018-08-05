e328df0eaa7bf12989158e3f5d5e886a044779e2
SolarisRig <33134468+solaris-rig@users.noreply.github.com>
2018-08-06 01:47:17 +0800

    Update SiteController.php
    Get remote IP

--- web/yaamp/modules/site/SiteController.php
+++ web/yaamp/modules/site/SiteController.php
@@ -3,14 +3,27 @@
 class SiteController extends CommonController
 {
 	public $defaultAction='index';
+	public function get_ip_address(){
+        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+            if (array_key_exists($key, $_SERVER) === true){
+                foreach (explode(',', $_SERVER[$key]) as $ip){
+                    $ip = trim($ip); // just to be safe
 
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                        return $ip;
+			    }
+			}
+		    }
+		}
+	    }
 	///////////////////////////////////////////////////
 	// Security Note: You can rename this action as you
 	// want, to customize the admin entrance url...
 	//
 	public function actionAdminRights()
 	{
-		$client_ip = arraySafeVal($_SERVER,'REMOTE_ADDR');
+		//$client_ip = arraySafeVal($_SERVER,'REMOTE_ADDR');
+		$client_ip = get_ip_address();
 		$valid = isAdminIP($client_ip);
 
 		if (arraySafeVal($_SERVER,'HTTP_X_FORWARDED_FOR','') != '') {
