72038a898dc7715b103448e97da6d4402e3bee61
SolarisRig <33134468+solaris-rig@users.noreply.github.com>
2018-12-22 06:04:33 +0800

    Update stratum.cpp

--- stratum/stratum.cpp
+++ stratum/stratum.cpp
@@ -134,11 +134,9 @@ YAAMP_ALGO g_algos[] =
 	{"jha", jha_hash, 0x10000, 0},
 
 	{"allium", allium_hash, 0x100, 0, 0},
-	{"lyra2", lyra2re_hash, 0x80, 0, 0},
+	{"lyra2", lyra2v2_hash, 0x100, 0, 0},
 	{"lyra2v2", lyra2v2_hash, 0x100, 0, 0},
-	{"lyra2v2-M", lyra2v2_hash, 0x100, 0, 0},
-	{"lyra2v2-H", lyra2v2_hash, 0x100, 0, 0},
-	{"lyra2z", lyra2z_hash, 0x100, 0, 0},
+	{"lyra2z", lyra2v2_hash, 0x100, 0, 0},
 
 	{"bastion", bastion_hash, 1, 0 },
 	{"blake", blake_hash, 1, 0 },
