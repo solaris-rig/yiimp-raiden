f5ace66f2056106cc6eff16d1a9c2531129e11c2
matrix-pay <matrixpay.dev@gmail.com>
2019-02-15 01:23:22 +0800

    Add Lyra2v3

--- stratum/algos/lyra2re3.c
+++ stratum/algos/lyra2re3.c
@@ -35,6 +35,7 @@
 #include "../sha3/sph_blake.h"
 #include "../sha3/sph_cubehash.h"
 #include "../sha3/sph_bmw.h"
+#include "Lyra2.h"
 
 void lyra2re3_hash(const char* input, char* output, uint32_t len)
 {
@@ -46,19 +47,19 @@ void lyra2re3_hash(const char* input, char* output, uint32_t len)
 
 	sph_blake256_init(&ctx_blake);
     sph_blake256(&ctx_blake, input, 80);
-    sph_blake256_close (&ctx_blake, hashA);	
+    sph_blake256_close (&ctx_blake, hashA);
 
     LYRA2_3(hashB, 32, hashA, 32, hashA, 32, 1, 4, 4);
-    
+
     sph_cubehash256_init(&ctx_cubehash);
     sph_cubehash256(&ctx_cubehash, hashB, 32);
     sph_cubehash256_close(&ctx_cubehash, hashA);
-    
+
     LYRA2_3(hashB, 32, hashA, 32, hashA, 32, 1, 4, 4);
-    
+
     sph_bmw256_init(&ctx_bmw);
     sph_bmw256(&ctx_bmw, hashB, 32);
     sph_bmw256_close(&ctx_bmw, hashA);
-    
+
    	memcpy(output, hashA, 32);
 }
